1e9aa51c4322b370411b260816ca348caddfde0b
Johnson <sakajohnson1@gmail.com>
2025-04-12 13:31:15 +0000

    added a lightbox gallery

---
+++ index.js
@@ -0,0 +1,25 @@
+// Get elements
+const mainImg = document.querySelector('.main-product-img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeLightbox = document.getElementById('closeLightbox');
+const thumbnails = document.querySelectorAll('.lightbox-thumbnails .thumb');
+
+// Open Lightbox
+mainImg.addEventListener('click', () => {
+  lightbox.style.display = 'flex';
+  lightboxImg.src = mainImg.src;
+});
+
+// Close Lightbox
+closeLightbox.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+// Switch Lightbox Image
+thumbnails.forEach(thumbnail => {
+  thumbnail.addEventListener('click', () => {
+    const newImgSrc = thumbnail.getAttribute('data-img');
+    lightboxImg.src = newImgSrc;
+  });
+});
